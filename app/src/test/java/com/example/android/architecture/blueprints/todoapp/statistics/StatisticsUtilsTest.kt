360dc6e3132045f2b721446ab4495170b274ec8f
rodriguesporan <rodriguesporan@gmail.com>
2021-09-09 20:03:04 -0300

    feat: added hamcrest dependency

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -1,7 +1,8 @@
 package com.example.android.architecture.blueprints.todoapp.statistics
 
 import com.example.android.architecture.blueprints.todoapp.data.Task
-import org.junit.Assert.assertEquals
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
 import org.junit.Test
 
 class StatisticsUtilsTest {
@@ -15,7 +16,7 @@ class StatisticsUtilsTest {
         val result = getActiveAndCompletedStats(tasks)
 
         // Check result
-        assertEquals(result.activeTasksPercent, 100f)
-        assertEquals(result.completedTasksPercent, 0f)
+        assertThat(result.activeTasksPercent, `is`(100f))
+        assertThat(result.completedTasksPercent, `is`(0f))
     }
 }
