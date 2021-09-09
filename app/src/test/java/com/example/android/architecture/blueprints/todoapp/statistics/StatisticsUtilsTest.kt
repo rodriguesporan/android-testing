f54692d7fb0ed1cbc9d9c1f8fe57357378b44824
rodriguesporan <rodriguesporan@gmail.com>
2021-09-09 20:25:16 -0300

    feat: written some tests

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -19,4 +19,30 @@ class StatisticsUtilsTest {
         assertThat(result.activeTasksPercent, `is`(100f))
         assertThat(result.completedTasksPercent, `is`(0f))
     }
+
+    @Test
+    fun getActiveAndCompletedStats_oneCompletedNoActive_returnsHundredZero() {
+        val tasks = listOf<Task>(Task("title", "desc", true))
+
+        val result = getActiveAndCompletedStats(tasks)
+
+        assertThat(result.activeTasksPercent, `is`(0f))
+        assertThat(result.completedTasksPercent, `is`(100f))
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_twoCompletedThreeActive_returnsFourthSixth() {
+        val tasks = listOf<Task>(
+            Task("title complete", "desc", true),
+            Task("title complete", "desc", true),
+            Task("title active", "desc", false),
+            Task("title active", "desc", false),
+            Task("title active", "desc", false)
+        )
+
+        val result = getActiveAndCompletedStats(tasks)
+
+        assertThat(result.activeTasksPercent, `is`(60f))
+        assertThat(result.completedTasksPercent, `is`(40f))
+    }
 }
