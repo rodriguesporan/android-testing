6bcf62a7094f6152f462eb37b2a0e32e4aa89699
rodriguesporan <rodriguesporan@gmail.com>
2021-09-11 08:12:32 -0300

    feat: added test case to viewModel

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,5 +1,22 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
-import org.junit.Assert.*
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
 
-class TasksViewModelTest
+@RunWith(AndroidJUnit4::class)
+class TasksViewModelTest {
+
+    @Test
+    fun addNewTask_setsNewTaskEvent() {
+        // Given a fresh TasksViewModel
+        val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+
+        // When adding a new task
+        tasksViewModel.addNewTask()
+
+        // Then the new task event is triggered
+        // TODO test LiveData
+    }
+}
